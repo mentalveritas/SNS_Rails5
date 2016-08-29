a89f653cd296f7dbf479cae321f4826a46de6785
mentalveritas <mentalveritas@gmail.com>
2016-08-29 14:33:34 +0900

    Enhance like async

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+
   def like_toggle
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-
+    @li_post = Post.find_by(id: params[:post_id])
+    @like = like
+    @curren_u = current_user
     if like.nil?
       Like.create(user_id: current_user.id, post_id: params[:post_id])
     else
       like.destroy
     end
-    
-    redirect_to :back
 
   end
 end
