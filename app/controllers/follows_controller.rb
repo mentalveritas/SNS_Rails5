bef70032d302494e32d683fcaf7a442221325c29
mentalveritas <mentalveritas@gmail.com>
2016-08-31 09:47:35 +0900

    Enhance follow async

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,13 +1,23 @@
 class FollowsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_post
 
   def create
     Follow.create(followed_id: params[:followed_id], follower_id: current_user.id)
-    redirect_to :back
+    @post_user_id = params[:followed_id]
+    @post_create = Post.find_by(id: params[:followed_id])
   end
 
   def destroy
     Follow.find_by(followed_id: params[:id], follower_id: current_user.id).destroy
-    redirect_to :back
+    @post_user_id = params[:id]
+    @post_destroy = Post.find_by(id: params[:id])
   end
+
+  private
+
+  def check_post
+    @current_u = current_user
+  end
+
 end
