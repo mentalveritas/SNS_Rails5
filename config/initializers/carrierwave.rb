7e8a5ab327b044c2d369b47381061e7655c54cec
mentalveritas <mentalveritas@gmail.com>
2016-09-01 17:33:44 +0900

    AWS Setting2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,7 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    region:            ENV.fetch('ap-northeast-2')
+
   }
 end if Rails.env == 'production'
-  storage :aws
-else
-  storage :file
-end
